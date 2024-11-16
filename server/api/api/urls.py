2218e5a7ba5252fdca8bde3c7dd84b7660c7088f
Arian Hadi <arian.hadi78@gmail.com>
2024-11-16 11:08:07 +0100

    added the urls to python

--- server/api/api/urls.py
+++ server/api/api/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include,re_path
+from rest_framework import routers
+from apiapp.views import UserViewSet
+
+router = routers.defaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/users', include('dj_rest_auth.urls')),
+    path('api/users/signup', include('dj_rest_auth.registration.urls')),
 ]
